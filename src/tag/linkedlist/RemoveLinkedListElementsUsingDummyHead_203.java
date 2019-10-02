40473f40abccd33ed50dc4538be8a04809187efb
Haoming Chen <haomingc1993@gmail.com>
2019-10-03 01:01:42 +0800

    1. 引入虚拟头节点实现列表移除指定元素代码修改

--- src/tag/linkedlist/RemoveLinkedListElementsUsingDummyHead_203.java
+++ src/tag/linkedlist/RemoveLinkedListElementsUsingDummyHead_203.java
@@ -11,11 +11,14 @@ public class RemoveLinkedListElementsUsingDummyHead_203 {
         ListNode prev = dummyHead;
         while (prev != null) {
             if (prev.next.val == val) {
+                //当dummyHead.next与val匹配时
+                //以下代码实际上改变的是dummyhead的next指向的节点，而head节点没有变化,head被dummyhead.next替代
+                //head在第一次匹配过后即变为loitering objects
                 prev.next = prev.next.next;
             } else {
                 prev = prev.next;
             }
         }
-        return head;
+        return dummyHead.next;
     }
 }
