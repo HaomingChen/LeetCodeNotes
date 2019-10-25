4e15cd05e556e3f828bde4640f9e7c102f14d023
HaomingChen <haomingc1993@gmail.com>
2019-10-25 17:16:44 +0800

    1. 二分搜索树到更大和树补充注释

--- src/tag/bst/BinarySearchTreeToGreaterSumTree_1038.java
+++ src/tag/bst/BinarySearchTreeToGreaterSumTree_1038.java
@@ -18,14 +18,17 @@ public class BinarySearchTreeToGreaterSumTree_1038 {
 
     int sum = 0;
 
+    //中序遍历, 深度优先
     public TreeNode bstToGst(TreeNode root) {
         if (root == null) {
             return null;
         }
+        //首先到达右子树的最深部分
         bstToGst(root.right);
         sum = sum + root.val;
         root.val = sum;
         bstToGst(root.left);
+        //返回根节点
         return root;
     }
 
