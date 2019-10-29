7695919e62337f7d0955b3c7317150057bd1677f
Haoming Chen <haomingc1993@gmail.com>
2019-10-29 23:37:38 +0800

    1. 归并排序框架

---
+++ src/algorithm/sortbasic/MergeSort.java
@@ -0,0 +1,28 @@
+package algorithm.sortbasic;
+
+/**
+ * 归并排序
+ *
+ * @author 58212
+ * @date 2019-10-29 23:30
+ */
+public class MergeSort<E extends Comparable<E>> {
+
+    public static void main(String[] args) {
+    }
+
+    public void sort(E[] arr, int n) {
+//        mergeSort();
+    }
+
+    //归并当前数组
+    private void merge(E[] arr, int l, int mid, int r) {
+
+    }
+
+    //归并排序
+    private void mergeSort(E[] arr, int l, int r) {
+
+    }
+
+}
