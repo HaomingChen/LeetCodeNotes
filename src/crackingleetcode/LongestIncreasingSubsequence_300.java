559a940e61679a8b54724c537661c7996f1ec76e
HaomingChen <haomingc1993@gmail.com>
2020-01-21 16:01:31 +0800

    1. 字符串dp问题暂时无法理解

--- src/crackingleetcode/LongestIncreasingSubsequence_300.java
+++ src/crackingleetcode/LongestIncreasingSubsequence_300.java
@@ -1,7 +1,5 @@
 package crackingleetcode;
 
-import java.util.Arrays;
-
 /**
  * Given an unsorted array of integers, find the length of longest increasing subsequence.
  *
@@ -9,23 +7,5 @@ import java.util.Arrays;
  * @date 2020-01-20 1:31
  */
 public class LongestIncreasingSubsequence_300 {
-
-    public int lengthOfLIS(int[] nums) {
-        //dp[i] = max(1 + dp[0, i - 1])
-        int[] dp = new int[nums.length];
-        if (nums.length == 0 || nums.length == 1) {
-            return nums.length;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-            for (int j = i; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                }
-            }
-        }
-        Arrays.sort(dp);
-        return dp[dp.length - 1];
-    }
 
 }
--- src/crackingleetcode/LongestPalindromicSubsequence_516.java
+++ src/crackingleetcode/LongestPalindromicSubsequence_516.java
@@ -18,17 +18,14 @@ public class LongestPalindromicSubsequence_516 {
 
         for (int i = s.length() - 1; i >= 0; i--) {
             dp[i][i] = 1;
-            for (int j = i+1; j < s.length(); j++) {
+            for (int j = i + 1; j < s.length(); j++) {
                 if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i+1][j-1] + 2;
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
                 } else {
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
                 }
-                System.out.print(dp[i][j] + " ");
             }
-            System.out.println();
         }
-        System.out.println("res: " + dp[0][s.length() - 1]);
-        return dp[0][s.length()-1];
+        return dp[0][s.length() - 1];
     }
 }
