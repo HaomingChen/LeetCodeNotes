c295903c45b3822efe2754de539dae679220798c
HaomingChen <haomingc1993@gmail.com>
2020-02-06 02:18:32 +0800

    1. 重建二叉树的船新版本

--- src/algorithm/frequent/ReConstructBinaryTree.java
+++ src/algorithm/frequent/ReConstructBinaryTree.java
@@ -9,20 +9,21 @@ import java.util.Arrays;
  * @date 2020-01-27 23:48
  */
 public class ReConstructBinaryTree {
-   public TreeNode reConstructBinaryTree(int[] pre, int[] in){
-       if(pre.length == 0 || in.length == 0){
-           return null;
-       }
-       TreeNode root = new TreeNode(pre[0]);
-       for(int i = 0; i < in.length; i++){
-           if(pre[0] == in[i]){
-               root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1),
-                       Arrays.copyOfRange(in, 0, i));
-               root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length),
-                       Arrays.copyOfRange(in, i + 1, in.length));
-               break;
-           }
-       }
-       return root;
-   }
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre.length == 0 || in.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[0]);
+        int i = 0;
+        for (; i < in.length; i++) {
+            if (in[i] == pre[0]) {
+                break;
+            }
+        }
+        root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1),
+                Arrays.copyOfRange(in, 0, i));
+        root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length),
+                Arrays.copyOfRange(in, i + 1, in.length));
+        return root;
+    }
 }
