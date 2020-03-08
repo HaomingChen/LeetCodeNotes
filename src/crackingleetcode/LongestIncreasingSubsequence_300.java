eed1b8bc0b8c249b6d167934676ed8768ab8451f
HaomingChen <haomingc1993@gmail.com>
2020-03-09 02:14:59 +0800

    1. leetcode 300 bst + dp logn

--- src/crackingleetcode/LongestIncreasingSubsequence_300.java
+++ src/crackingleetcode/LongestIncreasingSubsequence_300.java
@@ -8,24 +8,62 @@ package crackingleetcode;
  */
 public class LongestIncreasingSubsequence_300 {
 
+    //bst + dp -> nlogn
     public int lengthOfLIS(int[] nums) {
-        if(nums.length == 0){
-            return 0;
+
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
         }
-        int max = 1;
-        int[] dp = new int[nums.length];
-        dp[0] = 1;
-        for(int i = 1; i < nums.length; i++){
-            int res = 1;
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    res = Math.max(res, dp[j] + 1);
-                    max = Math.max(res, max);
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        int size = 1;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > dp[size - 1]) {
+                dp[size] = nums[i];
+                size++;
+                continue;
+            }
+            int l = 0;
+            int r = size - 1;
+            while (l < r - 1) {
+                int mid = (l + r) / 2;
+                if (nums[i] < dp[mid]) {
+                    r = mid;
+                } else {
+                    l = mid;
                 }
             }
-            dp[i] = res;
+            if (dp[l] < nums[i] && dp[l + 1] > nums[i]) {
+                dp[l + 1] = nums[i];
+            } else if (dp[l] >= nums[i]) {
+                dp[l] = nums[i];
+            } else {
+                dp[r] = nums[i];
+            }
         }
-        return max;
+        return size;
+
     }
+
+//    public int lengthOfLIS(int[] nums) {
+//        if(nums.length == 0){
+//            return 0;
+//        }
+//        int max = 1;
+//        int[] dp = new int[nums.length];
+//        dp[0] = 1;
+//        for(int i = 1; i < nums.length; i++){
+//            int res = 1;
+//            for(int j = 0; j < i; j++){
+//                if(nums[j] < nums[i]){
+//                    res = Math.max(res, dp[j] + 1);
+//                    max = Math.max(res, max);
+//                }
+//            }
+//            dp[i] = res;
+//        }
+//        return max;
+//    }
 
 }
