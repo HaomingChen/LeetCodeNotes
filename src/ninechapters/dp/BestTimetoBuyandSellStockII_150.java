e012bfe4e415b8035cf68f9e6ec2fbfbac700ac8
haomingc <haomingc1993@gmail.com>
2020-07-23 23:32:06 +0800

    1. 股票问题第二题状态机解法

--- src/ninechapters/dp/BestTimetoBuyandSellStockII_150.java
+++ src/ninechapters/dp/BestTimetoBuyandSellStockII_150.java
@@ -8,19 +8,33 @@ package ninechapters.dp;
  */
 public class BestTimetoBuyandSellStockII_150 {
 
-    public int maxProfit(int[] prices) {
-        if (prices.length == 0) {
-            return 0;
+    /**
+     * finite state machine approach
+     */
+    public int maxProfit(int[] prices){
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
         }
-        int res = 0;
-        int price = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                res = res + prices[i] - prices[i - 1];
-            }
-            price = prices[i];
-        }
-        return res;
+        return dp_i_0;
     }
+
+//    public int maxProfit(int[] prices) {
+//        if (prices.length == 0) {
+//            return 0;
+//        }
+//        int res = 0;
+//        int price = prices[0];
+//        for (int i = 1; i < prices.length; i++) {
+//            if (prices[i] > prices[i - 1]) {
+//                res = res + prices[i] - prices[i - 1];
+//            }
+//            price = prices[i];
+//        }
+//        return res;
+//    }
 
 }
