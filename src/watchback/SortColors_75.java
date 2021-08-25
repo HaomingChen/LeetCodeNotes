6ee9baf254a1b8edf373c1315c8b01088c76b1c1
haomingc <haomingc1993@gmail.com>
2021-08-25 13:02:18 +0800

    sort colors optimize

--- src/watchback/SortColors_75.java
+++ src/watchback/SortColors_75.java
@@ -5,15 +5,7 @@ public class SortColors_75 {
     public void sortColors(int[] nums) {
         int[] sum = new int[3];
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                sum[0]++;
-            }
-            if (nums[i] == 1) {
-                sum[1]++;
-            }
-            if (nums[i] == 2) {
-                sum[2]++;
-            }
+            sum[nums[i]]++;
         }
         int start = 0;
         for (int i = 0; i < sum.length; i++) {
